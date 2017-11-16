880f69b47dec0a14ee56fbc0d6af45bf19912b3e
Dedekind561 <mitchelllloyd154@gmail.com>
2017-11-16 09:07:03 +0000

    remove function solutions

--- index.js
+++ index.js
@@ -1,80 +1,28 @@
 const hof = {};
 
-hof.identity = function (x) {
-  return x;
-};
+hof.identity = function () {};
 
-hof.identityf = function (x) {
-  return function() {
-    return x;
-  };
-};
+hof.identityf = function () {};
 
-hof.add = function (a,b) {
-  return a + b;
-};
+hof.add = function () {};
 
-hof.sub = function (a,b) {
-  return a - b;
-};
+hof.sub = function () {};
 
-hof.mul = function (a,b) {
-  return a*b;
-};
+hof.mul = function () {};
 
-hof.addf = function (a) {
-  return function(b) {
-    return a + b;
-  };
-};
+hof.addf = function () {};
 
-hof.liftf = function (bin) {
-  return function (a) {
-    return function(b) {
-      return bin(a,b);
-    };
-  };
-};
+hof.liftf = function () {};
 
-hof.curry = function (bin,a) {
-  return function(b) {
-    return bin(b,a);
-  };
-};
+hof.curry = function () {};
 
-// hof.inc = function (val) {
-//   return val+1;
-// };
+hof.inc = function () {};
 
-hof.inc = function (val) {
-  return hof.add(val,1);
-};
+hof.twice = function () {};
 
-hof.twice = function (bin) {
-  return function (a) {
-    return bin(a,a);
-  };
-};
+hof.reverse = function () {};
 
-hof.reverse = function (func) {
-  
-  return function () {
-    const args = [...arguments];
-    const newArgs = args.reverse();
-    return func(...newArgs);
-  };
-};
-
-hof.composeu = function () {
-
-  const args = [...arguments];
-  return function(val) {
-    return args.reduce((acc,func,i) => {
-      i === 0 ? acc = func(val) : acc = func(acc);
-      return acc;
-    },0);
-  };
-};
+hof.composeu = function () {};
 
 hof.composeb = function () {};
 
