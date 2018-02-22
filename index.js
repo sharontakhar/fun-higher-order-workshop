4ab170230f0b41eca4ab840bfd9d42e7bb34a5d5
Jac <jacdarby@gmail.com>
2018-02-22 09:54:10 +0000

    removes solutions

--- index.js
+++ index.js
@@ -1,74 +1,55 @@
 const hof = {};
 
-hof.identity = x => x;
+hof.identity = function () {};
 
-hof.identityf = x => () => x;
+hof.identityf = function () {};
 
-hof.add = (a, b) => a + b;
+hof.add = function () {};
 
-hof.sub = (x, y) => x - y;
+hof.sub = function () {};
 
-hof.mul = (x, y) => x * y;
+hof.mul = function () {};
 
-hof.addf = x => y => x + y;
+hof.addf = function () {};
 
-hof.liftf = func => x => y => func(x, y);
+hof.liftf = function () {};
 
-hof.curry = (operation, ...initialArgs) => (...remainingArgs) =>
-  operation(...initialArgs, ...remainingArgs);
+hof.curry = function () {};
 
-hof.inc = x => ++x;
+hof.inc = function () {};
 
-hof.twice = func => x => func(x, x);
+hof.twice = function () {};
 
-hof.reverse = func => (x, y) => func(y, x);
+hof.reverse = function () {};
 
-hof.composeu = (func1, func2) => x => func2(func1(x));
+hof.composeu = function () {};
 
-hof.composeb = (func1, func2) => (x, y, z) => func2(func1(x, y), z);
+hof.composeb = function () {};
 
-hof.limit = (func, limit) => {
-  let callCount = 0;
-  return (x, y) => (callCount++ < limit ? func(x, y) : undefined);
-};
+hof.limit = function () {};
 
-hof.from = x => () => x++;
+hof.from = function () {};
 
-hof.to = (gen, x) => hof.limit(gen, x);
+hof.to = function () {};
 
-hof.fromTo = (x, y) => hof.to(hof.from(x), y);
+hof.fromTo = function () {};
 
-hof.element = (arr, gen = hof.from(0)) => () => arr[gen()];
+hof.element = function () {};
 
-hof.collect = (gen, arr) => [...arr, gen()];
+hof.collect = function () {};
 
-hof.filter = (gen, predicate) => predicate(gen());
+hof.filter = function () {};
 
-hof.concat = (gen1, gen2) => () => {
-  const x = gen1();
-  return x !== undefined ? x : gen2();
-};
+hof.concat = function () {};
 
-hof.gensymf = char => {
-  let x = 0;
-  return () => `${char}${x++}`;
-};
+hof.gensymf = function () {};
 
-hof.gensymff = (gen, seed) => char => {
-  const value = gen(seed);
+hof.gensymff = function () {};
 
-  return () => `${char}${gen(value)}`;
-};
+hof.fibonaccif = function () {};
 
-hof.fibonaccif = function() {};
+hof.counter = function () {};
 
-hof.counter = x => ({ x, up: () => ++x, down: () => --x });
-
-hof.revokable = func => ({
-  invoke: func,
-  revoke () {
-    this.invoke = () => undefined;
-  }
-});
+hof.revokable = function () {};
 
 module.exports = hof;
