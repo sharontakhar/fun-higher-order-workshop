ffb95814b90551ef261a3eb7775e8a206ee74aef
Paul Copley <pkcopley@gmail.com>
2018-07-19 09:31:23 +0100

    Remove fibonaccif

--- index.js
+++ index.js
@@ -1,53 +1,51 @@
 const hof = {};
 
-hof.identity = function () {};
+hof.identity = function() {};
 
-hof.identityf = function () {};
+hof.identityf = function() {};
 
-hof.add = function () {};
+hof.add = function() {};
 
-hof.sub = function () {};
+hof.sub = function() {};
 
-hof.mul = function () {};
+hof.mul = function() {};
 
-hof.addf = function () {};
+hof.addf = function() {};
 
-hof.liftf = function () {};
+hof.liftf = function() {};
 
-hof.curry = function () {};
+hof.curry = function() {};
 
-hof.inc = function () {};
+hof.inc = function() {};
 
-hof.twice = function () {};
+hof.twice = function() {};
 
-hof.composeu = function () {};
+hof.composeu = function() {};
 
-hof.composeb = function () {};
+hof.composeb = function() {};
 
-hof.limit = function () {};
+hof.limit = function() {};
 
-hof.from = function () {};
+hof.from = function() {};
 
-hof.to = function () {};
+hof.to = function() {};
 
-hof.fromTo = function () {};
+hof.fromTo = function() {};
 
-hof.element = function () {};
+hof.element = function() {};
 
-hof.collect = function () {};
+hof.collect = function() {};
 
-hof.filter = function () {};
+hof.filter = function() {};
 
-hof.concat = function () {};
+hof.concat = function() {};
 
-hof.gensymf = function () {};
+hof.gensymf = function() {};
 
-hof.gensymff = function () {};
+hof.gensymff = function() {};
 
-hof.fibonaccif = function () {};
+hof.counter = function() {};
 
-hof.counter = function () {};
-
-hof.revokable = function () {};
+hof.revokable = function() {};
 
 module.exports = hof;
